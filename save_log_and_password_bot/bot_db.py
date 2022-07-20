42dbe061163cfb0f843b9a8d6670bb8fb51cf74d
piv7020 <109218488+piv7020@users.noreply.github.com>
2022-07-20 16:42:59 +0300

    Add files via upload

--- save_log_and_password_bot/bot_db.py
+++ save_log_and_password_bot/bot_db.py
@@ -12,34 +12,53 @@ def sql_start():
     base.commit()
 
 
-async def sql_add_account(login,password):
-    global base , cur
-    print(login,password)
-    cur.execute(f'INSERT INTO accounts VALUES(?, ?)',(login,password))
+async def sql_add_account(login, password):
+    global base, cur
+    print(login, password)
+    cur.execute(f'INSERT INTO accounts VALUES(?, ?)', (login, password))
     base.commit()
 
 
 async def delete_all_acc(message):
-    pass
+    global cur, base
+    try:
+        sql_update_query = 'DELETE from accounts'
+        cur.execute(sql_update_query)
+        base.commit()
+        await bot.send_message(message.from_user.id, 'все аккаунты удалены')
+    except sq.Error as error:
+        await bot.send_message(message.from_user.id, f'произошла ошибка в работе с базой данных:\n{error} ')
 
 
 async def sql_read_info(message):
     try:
-        acc_count=0
-        accounts=''
+        acc_count = 0
+        accounts = ''
 
         for info in cur.execute('SELECT * FROM accounts').fetchall():
             acc_count += 1
             accounts += f'\n Аккаунт: {acc_count}\nЛогин: {info[0]}Пароль:{info[1]}'
-        await bot.send_message(message.from_user.id,accounts)
+        await bot.send_message(message.from_user.id, accounts)
 
     except:
-        await bot.send_message(message.from_user.id,'Добавленных аккаунтов нет')
+        await bot.send_message(message.from_user.id, 'Добавленных аккаунтов нет')
 
 
+async def del_acc_db(message, login):
+    global cur, base
+    try:
+        count = 0
+        for log in cur.execute("SELECT * FROM accounts").fetchall():
+            if log[0] == login:
+                sql_udate_query = 'DELETE from accounts where login=?'
+                cur.execute(sql_udate_query, (login,))
+                base.commit()
+                await bot.send_message(message.from_user.id, f'аккаунт {login} успешно удалён')
+                count += 1
+                break
+        if count == 0:
+            await bot.send_message(message.from_user.id, f'аккаунт {login} не найден')
 
 
-
-
-
-
+    except sq.Error as error:
+        await bot.send_message(message.from_user.id, 'ошибка при  работе с базой данных')
--- save_log_and_password_bot/save_log_and_password_bot_main.py
+++ save_log_and_password_bot/save_log_and_password_bot_main.py
@@ -23,7 +23,9 @@ class FSMAdmin(StatesGroup):
     get_log = State()
     get_pass = State()
     accept = State()
-    get_info =State()
+    get_info = State()
+    delete_accounts = State()
+    delete_log=State()
 
 
 @dp.message_handler(Text(equals='отмена', ignore_case=True), state='*')
@@ -32,7 +34,33 @@ async def cancel(message: types.Message, state: FSMContext):
     await FSMAdmin.show_info.set()
 
 
-@dp.message_handler(Text(equals=['добавить аккаунт','получить информацию'], ignore_case=True))
+@dp.message_handler(Text(equals=['добавить аккаунт','получить информацию','удалить все аккаунты','удалить аккаунт'],
+                         ignore_case=True))
+async def start_choice(message: types.Message, state: FSMContext):
+    if message.text=='добавить аккаунт':
+        await bot.send_message(message.from_user.id, 'введите логин', reply_markup=u_s_k_b)
+        await FSMAdmin.get_log.set()
+
+    elif message.text == 'удалить все аккаунты':
+        await delete_all_acc(message)
+        await state.finish()
+
+    elif message.text == 'удалить аккаунт':
+        await bot.send_message(message.from_user.id, 'введите аакаунт ,который ты хочешь удалить',
+                               reply_markup=cancel_but)
+        await FSMAdmin.delete_log.set()
+
+    else:
+        await sql_read_info(message=message)
+        await state.finish()
+
+
+@dp.message_handler(state=FSMAdmin.delete_log)
+async def delete_acc(message:types.Message,state:FSMContext):
+    await del_acc_db(message=message, login=message.text)
+    await state.finish()
+
+
 @dp.message_handler(commands='start')
 async def start_mes(message: types.Message, state: FSMContext):
     if message.from_user.id == admin_id:
@@ -47,6 +75,16 @@ async def send_inf(message: types.Message, state: FSMContext):
     if message.text == 'получить информацию':
         await sql_read_info(message=message)
         await state.finish()
+
+    elif message.text == 'удалить все аккаунты':
+        await delete_all_acc(message)
+        await state.finish()
+
+    elif message.text == 'удалить аккаунт':
+        await bot.send_message(message.from_user.id, 'введите аакаунт ,который ты хочешь удалить',
+                               reply_markup=cancel_but)
+        await FSMAdmin.delete_log.set()
+
     else:
         await bot.send_message(message.from_user.id, 'введите логин', reply_markup=cancel_but)
         await FSMAdmin.get_log.set()
@@ -62,7 +100,7 @@ async def get_log(message: types.Message, state: FSMContext):
 
 @dp.message_handler(state=FSMAdmin.get_pass)
 async def get_pas(message: types.Message, state: FSMContext):
-    global login, password
+    global login , password
     async with state.proxy() as data:
         data['password'] = message.text
     login = data.get('login')
@@ -74,29 +112,33 @@ async def get_pas(message: types.Message, state: FSMContext):
 
 @dp.message_handler(state=FSMAdmin.accept)
 async def accept(message: types.Message, state: FSMContext):
-    global login, password, users
-    try:
-        await sql_add_account(login=login, password=password)
-        await bot.send_message(message.from_user.id, 'ваши данные успешно сохранены',
-                               reply_markup=u_s_k_b)
-    except Exception as ex:
-        print(repr(ex))
-        await bot.send_message(message.from_user.id, 'такой логин уже есть',
-                               reply_markup=u_s_k_b)
-        await FSMAdmin.show_info.set()
-    # if message.text=='да':
-    #     users[login] = password
-    #     print(users)
-    #     await state.finish()
-    # else:
-    #     await bot.send_message(message.from_user.id, f'ввод отменён выберите действие',reply_markup=u_s_k_b)
-    #     await FSMAdmin.show_info.set()
+    global login , password , users
+    if message.text=='да':
+        try:
+            await sql_add_account(login=login, password=password)
+            await bot.send_message(message.from_user.id, 'ваши данные успешно сохранены',
+                                   reply_markup=u_s_k_b)
+            await state.finish()
+        except Exception as ex:
+            print(repr(ex))
+            await bot.send_message(message.from_user.id, 'такой логин уже есть',
+                                   reply_markup=u_s_k_b)
+            await FSMAdmin.show_info.set()
+        # if message.text=='да':
+        #     users[login] = password
+        #     print(users)
+        #     await state.finish()
+        # else:
+        #     await bot.send_message(message.from_user.id, f'ввод отменён выберите действие',reply_markup=u_s_k_b)
+        #     await FSMAdmin.show_info.set()
 
 
 # '''*********************************************BUTTONS********************************************'''
 
 u_s_k_b = ReplyKeyboardMarkup(resize_keyboard=True).add(KeyboardButton('добавить аккаунт'))\
-                                                        .add(KeyboardButton('получить информацию'))
+    .add(KeyboardButton('получить информацию'))\
+    .add(KeyboardButton('удалить все аккаунты'))\
+    .add(KeyboardButton('удалить аккаунт'))
 
 cancel_but = ReplyKeyboardMarkup(resize_keyboard=True).add(KeyboardButton('отмена'))
 
